af8a3938cac311be064c011c5c7c8d77b30864d1
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-09-22 11:07:56 +0200

    feat: enabled and fixed husky precommit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,19 @@
-import React, { FC } from "react";
-import * as S from "./styles";
+import React, { FC } from 'react'
+import * as S from './styles'
 
-import { ButtonProps } from "./types";
+import { ButtonProps } from './types'
 
 const Button: FC<ButtonProps> = ({ label, onClick, category, active }) => {
   const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    onClick();
-  };
+    event.preventDefault()
+    onClick()
+  }
 
   return (
     <S.Button onClick={buttonHandler} active={active} category={category}>
       {label}
     </S.Button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
