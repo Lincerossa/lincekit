8858c008c4f1523a9358816a23bc779945f0d433
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-09-22 11:18:40 +0200

    fix: removed bugged vite eslint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
-import path from "node:path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
@@ -10,24 +9,23 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
-    eslint(),
   ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/components/index.tsx"),
-      name: "lincekit",
-      formats: ["es", "umd"],
+      entry: path.resolve(__dirname, 'src/components/index.tsx'),
+      name: 'lincekit',
+      formats: ['es', 'umd'],
       fileName: (format) => `lincekit.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom", "styled-components"],
+      external: ['react', 'react-dom', 'styled-components'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "styled-components": "styled",
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
         },
       },
     },
   },
-});
+})
